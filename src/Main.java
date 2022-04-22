b856ae7694982a29099c3d9976a9654d18a528c0
Андрей <andrews.ru83@gmail.com>
2022-04-22 16:01:05 +0300

    Задание 4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет");
-    }
-}
+        short deliveryDistance = 95 ;
+        short distance1Day = 20 ;
+        short distance2Day = 60 ;
+        short distance3Day = 100 ;
+    if (deliveryDistance <= distance1Day) {
+        System.out.println("Потребуется дней: 1");
+    } else if (deliveryDistance <= distance2Day) {
+        System.out.println("Потребуется дней: 2");
+    } else {
+        System.out.println("Потребуется дней: 3");
+
+    }}}
