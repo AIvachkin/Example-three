06448c2b5b2eb7417b8d14f834ede273d6749926
Андрей <andrews.ru83@gmail.com>
2022-04-22 15:35:21 +0300

    Задание 3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет");
-    }
-}
+        short year = 2108 ;
+        float year400 = year % 400 ;
+        float year100 = year % 100 ;
+        float year4 = year % 4 ;
+
+    if (year400 == 0) {
+        System.out.println(year + " год является високосным");
+    } else if (year100 == 0) {
+        System.out.println(year + " год не является високосным");
+    } else if (year4 == 0) {
+        System.out.println(year + " год является високосным");
+    } else {
+        System.out.println(year + " год не является високосным");
+    }}}
